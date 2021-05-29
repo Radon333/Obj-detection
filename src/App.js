1171a8cff17eae990124b71bb4aef28c41467a33
= <=>
2021-05-29 17:59:49 +0530

    Image Based Detection Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react'
+import "./App.css";
+import styled from "styled-components";
+import {ImageBased} from './Pages/ImageBased/ImageBased'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #1c2127;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+`;
+
+function App(){
+  return (
+    <AppContainer>
+      <ImageBased />
+    </AppContainer>
+  )
 }
 
-export default App;
+export default App
