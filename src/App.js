32fb9070197c2c6684740c0365606b4563c84bcd
Ranveer Shah <ranveer2001s@gmail.com>
2021-06-01 16:57:34 +0530

    Upgraded App.js
    contains support for various components and pages  with router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "./App.css";
-import styled from "styled-components";
+import Navbar from "./Components/Navbar/Navbar"
+import Footer from "./Components/Footer/Footer"
 import {ImageBased} from './Pages/ImageBased/ImageBased'
-import { Button } from 'react-bootstrap';
-import {LiveBased} from './Pages/LiveBased/LiveBased'
+import Button  from 'react-bootstrap';
+import LiveBased from './Pages/LiveBased/LiveBased'
+import Help from './Pages/Help/Help'
+import Home from "./Pages"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
-const AppContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: #1c2127;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: #fff;
-`;
-
-function App(){
+function App() { 
   return (
-    <AppContainer>
-      <ImageBased />
-    </AppContainer>
-  )
-}
-
-export default App
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/live-camera-based' component={LiveBased} />
+        <Route path='/image-based' component={ImageBased} />
+        <Route path='/help' component={Help} />
+        </Switch>
+        <Footer/>
+    </Router>
+  );
+} 
+  
+export default App; 
