8b330f4ca816130b52ce74ef91d4fc9b1d26e426
Ranveer Shah <ranveer2001s@gmail.com>
2021-06-01 16:52:23 +0530

    Home page with router support

---
+++ src/Pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Home/Home.css"
+
+function Home() {
+  return (
+    <div>
+                  <div className="hero-area">
+                  <div className="hero-text">
+                  <h1>Home</h1>
+                  </div>
+                  </div> 
+    </div>
+  )
+}
+
+export default Home
