0084ec5660c2cb2d391e87176eb564252b967da0
Ranveer Shah <ranveer2001s@gmail.com>
2021-06-01 16:49:47 +0530

    Added React Router support for navigation links
    css improvements

---
+++ src/Components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems=[
+
+    {
+        title:'Home',
+        url:'/',
+        cName:"nav-links"
+    },
+    {
+        title:'Image-Based',
+        url:'/image-based',
+        cName:"nav-links"
+    },
+    {
+        title:'Live-Camera Based',
+        url:'/live-camera-based',
+        cName:"nav-links"
+    },
+    {
+        title:'Help',
+        url:'/help',
+        cName:"nav-links"
+    },
+]
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,1 +1,37 @@
-//Navbar
+import React, { Component } from 'react'
+import {MenuItems} from './MenuItems'
+import "./Navbar.css"
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
+
+class Navbar extends Component 
+{
+  state={clicked:false}
+  handleClick = () => {
+    this.setState({clicked:!this.state.clicked})
+  }
+  render() 
+  {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">Object Detetction</h1>
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i className={this.state.clicked ? 'fa fa-times' : 'fa fa-bars'}></i>
+        </div>
+        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+        {MenuItems.map((item,index) => {
+          return (
+            <li key={index}>
+              <Link className={item.cName} to={item.url} activeStyle>{item.title}</Link>
+            </li>
+          )
+        })}
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
