130508fffecd66f71bf0c11848326b80b27577bf
Ranveer Shah <ranveer2001s@gmail.com>
2021-06-04 00:21:04 +0530

    major changes
    improved navbar and homepage and complete routing support for buttons

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <div>
+      <footer className="footer">
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Pages/Help/Help.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Help.css"
+
+function Help() {
+  return (
+    <div>
+                  <div className="hero-area">
+                  <div className="hero-text">
+                  <h1>Help</h1>
+                  </div>
+                  </div> 
+    </div>
+  )
+}
+
+export default Help
